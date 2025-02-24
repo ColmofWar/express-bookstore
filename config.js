5b2654e51657503a1ba6baf56894e06aef233715
ColmofWar <joshuaallendimarco@gmail.com>
2025-02-24 03:35:26 -0500

    functional with test

--- config.js
+++ config.js
@@ -1,13 +1,15 @@
 /** Common config for bookstore. */
 
+require("dotenv").config();
 
-let DB_URI = `postgresql://`;
+let DB_URI = `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@localhost`;
 
 if (process.env.NODE_ENV === "test") {
   DB_URI = `${DB_URI}/books-test`;
 } else {
   DB_URI = process.env.DATABASE_URL || `${DB_URI}/books`;
 }
+
 
 
 module.exports = { DB_URI };
